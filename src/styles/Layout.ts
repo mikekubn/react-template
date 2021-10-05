31fb06af36f9b273d78ae4869b79cc95a9ac9109
Michael Kubín <Michael.kubin@livesporttv.cz>
2021-10-05 11:50:14 +0200

    feat: add/ basic setup, eslint, styled-components

---
+++ src/styles/Layout.ts
@@ -0,0 +1,20 @@
+import styled  from 'styled-components';
+
+export const Layout = styled.div`
+   height: 100vh;
+   width: 100%;
+   display: flex;
+   justify-content: center;
+`;
+
+export const Navigation = styled.div`
+   top: 20px;
+   background-color: black;
+   height: 30px;
+   width: 900px;
+`;
+
+export const MiddleLayout = styled.div`
+   background-color: red;
+   width: 1200px;
+`;
