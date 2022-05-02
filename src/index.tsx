b42f9658ae5da8792c0e01c0ed2f84282dfae990
Michael Kubín <Michael.kubin@livesporttv.cz>
2022-05-02 12:01:11 +0200

    fix: react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const appElementId = 'root';
+const container = document.getElementById(appElementId) as Element;
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
